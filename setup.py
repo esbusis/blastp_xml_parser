fd1928bab1581b3f287ed9b308047e2cdb518e77
esbusis <esbusraisik@gmail.com>
2022-06-03 14:16:23 +0300

    first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='esra',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='esbusis',
+author_email='esbusraisik@gmail.com',
+license='MIT',
+packages=['esra'],
+zip_safe=False)
