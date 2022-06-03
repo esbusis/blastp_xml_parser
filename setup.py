2960b655d95e1405553b587f7ddbe7b5643ede7d
esbusis <esbusraisik@gmail.com>
2022-06-03 14:24:08 +0300

    third

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='parser',
 version='0.1',
 description='blastp_xml_parser',
-url='git@github.com:esbusis/blastp_xml_parser.git',
+url='https://github.com/esbusis/blastp_xml_parser.git',
 author='esbusis',
 author_email='esbusraisik@gmail.com',
 license='MIT',
